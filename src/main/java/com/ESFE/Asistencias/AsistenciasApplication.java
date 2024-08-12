857c24189a38c93b15c10263d05fe1830582ed90
Mena0904 <Walthermena0799@gmail.com>
2024-08-11 21:53:44 -0600

    avance grupo

--- src/main/java/com/ESFE/Asistencias/AsistenciasApplication.java
+++ src/main/java/com/ESFE/Asistencias/AsistenciasApplication.java
@@ -9,5 +9,4 @@ public class AsistenciasApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(AsistenciasApplication.class, args);
 	}
-
 }
--- src/main/java/com/ESFE/Asistencias/Controladores/GrupoController.java
+++ src/main/java/com/ESFE/Asistencias/Controladores/GrupoController.java
@@ -9,9 +9,12 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 import java.util.Optional;
@@ -40,5 +43,22 @@ public class GrupoController {
             model. addAttribute( "pageNumbers",pageNumbers);
         }
         return "grupo/index";
+    }
+    @GetMapping("/create")
+    public String create(Model model) {
+        model.addAttribute("grupo", new Grupo());
+        return "grupo/create";
+    }
+
+    @PostMapping("/save")
+    public String save(Grupo grupo, BindingResult result, Model model, RedirectAttributes attributes) {
+        if (result.hasErrors()) {
+            model.addAttribute("grupo", grupo);
+            attributes.addFlashAttribute("error", "No se puede guardar debido a un error");
+            return "grupo/create";
+        }
+        grupoService.CreaOeditar(grupo);
+        attributes.addFlashAttribute("msg", "Creado Correctamente");
+        return "redirect:/grupos";
     }
 }
